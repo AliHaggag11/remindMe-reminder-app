ee24a3c1953eb4fdf49918f841e0e318f27cdd1d
Kliton <barekliton@gmail.com>
2023-08-29 03:08:31 +0200

    init repo

---
+++ app/(dashboard)/page.tsx
@@ -0,0 +1,86 @@
+import CollectionCard from "@/components/CollectionCard";
+import CreateCollectionBtn from "@/components/CreateCollectionBtn";
+import SadFace from "@/components/icons/SadFace";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Skeleton } from "@/components/ui/skeleton";
+import { prisma } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs";
+import { Suspense } from "react";
+
+export default async function Home() {
+  return (
+    <>
+      <Suspense fallback={<WelcomeMsgFallback />}>
+        <WelcomMsg />
+      </Suspense>
+      <Suspense fallback={<div>Loading collections...</div>}>
+        <CollectionList />
+      </Suspense>
+    </>
+  );
+}
+
+async function WelcomMsg() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <div>error</div>;
+  }
+
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        Welcome, <br /> {user.firstName} {user.lastName}
+      </h1>
+    </div>
+  );
+}
+
+function WelcomeMsgFallback() {
+  return (
+    <div className="flex w-full mb-12">
+      <h1 className="text-4xl font-bold">
+        <Skeleton className="w-[180px] h-[36px]" />
+        <Skeleton className="w-[150px] h-[36px]" />
+      </h1>
+    </div>
+  );
+}
+
+async function CollectionList() {
+  const user = await currentUser();
+  const collections = await prisma.collection.findMany({
+    include: {
+      tasks: true,
+    },
+    where: {
+      userId: user?.id,
+    },
+  });
+
+  if (collections.length === 0) {
+    return (
+      <div className="flex flex-col gap-5">
+        <Alert>
+          <SadFace />
+          <AlertTitle>There are no collections yet!</AlertTitle>
+          <AlertDescription>
+            Create a collection to get started
+          </AlertDescription>
+        </Alert>
+        <CreateCollectionBtn />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <CreateCollectionBtn />
+      <div className="flex flex-col gap-4 mt-6">
+        {collections.map((collection) => (
+          <CollectionCard key={collection.id} collection={collection} />
+        ))}
+      </div>
+    </>
+  );
+}
---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Separator } from "@/components/ui/separator";
+import NavBar from "@/components/NavBar";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "RemindMe",
+  description: "Created by: @CodeWithKliton",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html
+        lang="en"
+        className={cn(inter.className, "dark")}
+        style={{
+          colorScheme: "dark",
+        }}
+      >
+        <body>
+          <ThemeProvider>
+            <div
+              className="
+             flex
+             min-h-screen
+             w-full
+             flex-col
+             items-center
+             dark:bg-black"
+            >
+              <NavBar />
+              <Separator />
+              <main className="flex flex-grow w-full justify-center items-center dark:bg-neutral-950">
+                {children}
+                <Toaster />
+              </main>
+            </div>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
