c4e5354017e1466963e66a41de07ad144c59f064
AliHaggag11 <Ali7aggag@gmail.com>
2023-11-02 14:28:34 +0200

    build script

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Logo() {
   return (
-    <h1 className="text-2xl font-bold bg-gradient-to-r from-red-500 to-orange-500 bg-clip-text text-transparent">
+    <h1 className="text-2xl font-bold bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500 bg-clip-text text-transparent">
       RemindMe
     </h1>
   );
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,12 +1,11 @@
 export enum CollectionColors {
-    sunset = "bg-gradient-to-r from-red-500 to-orange-500",
-    poppy = "bg-gradient-to-r from-rose-400 to-red-500",
-    rosebud = "bg-gradient-to-r from-violet-500 to-purple-500",
-    snowflake = "bg-gradient-to-r from-indigo-400 to-cyan-400",
-    candy = "bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500",
-    firtree = "bg-gradient-to-r from-emerald-500 to-emerald-900",
-    metal = "bg-gradient-to-r from-slate-500 to-slate-800",
-    powder = "bg-gradient-to-r from-violet-200 to-pink-200",
+    Sunset = "bg-gradient-to-r from-red-500 to-orange-500",
+    Poppy = "bg-gradient-to-r from-rose-400 to-red-500",
+    Rosebud = "bg-gradient-to-r from-violet-500 to-purple-500",
+    Borealis = "bg-gradient-to-r from-indigo-400 to-teal-400",
+    Candy = "bg-gradient-to-r from-yellow-400 via-pink-500 to-red-500",
+    Firtree = "bg-gradient-to-r from-emerald-500 to-emerald-900",
+    Metal = "bg-gradient-to-r from-slate-500 to-slate-800",
 }
 
 export type CollectionColor = keyof typeof CollectionColors;
